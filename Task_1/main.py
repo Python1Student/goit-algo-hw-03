aefbfb4def1bb28d79b57f320921a93211038a3c
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-06 20:59:38 +0000

    While

--- Task_1/main.py
+++ Task_1/main.py
@@ -1,1 +1,18 @@
+import re
 from datetime import datetime
+
+
+def get_days_from_today(date):
+    pass
+
+condition = True
+while condition:
+    date_input = input('Write date in format: YYYY-MM-DD\n>>> ')
+
+    pattern = r'\d{4}\-\d{2}\-\d{2}'
+    match = re.search(pattern, date_input)
+
+    if match and date_input == match.group(): 
+        condition = False
+        get_days_from_today(date_input)
+    else: print('Wrong format!')
