0bfcfb548c9fb28b57a73636c67746dee6acf1d8
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-06 21:44:10 +0000

    Comments_1

--- Task_1/main.py
+++ Task_1/main.py
@@ -1,27 +1,26 @@
 import re
 from datetime import datetime
 
-
+# Створюємо функцію для обчислення кількості днів між датами
 def get_days_from_today(date):
     pass
 
+# Робимо для того шоб потім нормально зупинити цикл
 condition = True
+# Пишемо за межами циклу шоб не повторювалося
 print('Write date in format: YYYY-MM-DD')
 
 while condition:
+    # Отримуємо ввод з клавіатури
     date_input = input('>>> ')
 
-    pattern = r'(\d{4})\-(\d{2})\-(\d{2})'
-    match   = re.search(pattern, date_input)
-
-    if match and date_input == match.group():
-
-        try:    datetime.strptime(date_input, '%Y-%m-%d')
-        except: 
-            print('Wrong date!')
-            continue
-        
-        condition = False
-        get_days_from_today(date_input)
-
-    else: print('Wrong format!')
+    # Перевіряємо чи правильно ввів користувач дату
+    try: datetime.strptime(date_input, '%Y-%m-%d')
+    except: 
+        print('Wrong date!')
+        continue
+    
+    # Ставимо False шоб більше не повторювався цикл перевірки
+    condition = False
+    # Нарешті викликаємо функцію для обчислення
+    get_days_from_today(date_input)
