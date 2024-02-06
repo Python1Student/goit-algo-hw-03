a39949c164f85395ca13625e120caed8d83619ec
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-06 21:31:10 +0000

    while_correct_date

--- Task_1/main.py
+++ Task_1/main.py
@@ -11,10 +11,17 @@ print('Write date in format: YYYY-MM-DD')
 while condition:
     date_input = input('>>> ')
 
-    pattern = r'\d{4}\-\d{2}\-\d{2}'
-    match = re.search(pattern, date_input)
+    pattern = r'(\d{4})\-(\d{2})\-(\d{2})'
+    match   = re.search(pattern, date_input)
 
-    if match and date_input == match.group(): 
+    if match and date_input == match.group():
+
+        try:    datetime.strptime(date_input, '%Y-%m-%d')
+        except: 
+            print('Wrong date!')
+            continue
+        
         condition = False
         get_days_from_today(date_input)
+
     else: print('Wrong format!')
