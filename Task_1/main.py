d6a7cc645029ea60f98448d2ac5e8c3d2dc3f9c1
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-06 21:01:32 +0000

    While_without_rule

--- Task_1/main.py
+++ Task_1/main.py
@@ -6,8 +6,10 @@ def get_days_from_today(date):
     pass
 
 condition = True
+print('Write date in format: YYYY-MM-DD')
+
 while condition:
-    date_input = input('Write date in format: YYYY-MM-DD\n>>> ')
+    date_input = input('>>> ')
 
     pattern = r'\d{4}\-\d{2}\-\d{2}'
     match = re.search(pattern, date_input)
