78d43a330452452da88d4449a04de7048c1a449c
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-09 07:25:40 +0000

    Added_index

--- Task_3/main.py
+++ Task_3/main.py
@@ -6,7 +6,13 @@ def normalize_phone(phone_number: str) -> str:
     pattern = r'\D'
     replacement = r''
     formated_number = re.sub(pattern, replacement, phone_number)
-    formated_number = '+' + formated_number
+
+    ukraine_index = '380'
+    find_ukraine_index = re.search(ukraine_index, formated_number)
+    
+    if find_ukraine_index.span == (0, 3):
+        formated_number = '+' + formated_number
+    else: formated_number = '+38' + formated_number
     
     return formated_number
 
@@ -14,7 +20,7 @@ def normalize_phone(phone_number: str) -> str:
 # Робимо для того шоб потім нормально зупинити цикл
 condition = True
 # Пишемо за межами циклу шоб не повторювалося
-print('Enter number: ')
+print('Enter phone number: ')
 
 while condition:
     phone_number = input('>>> ')
