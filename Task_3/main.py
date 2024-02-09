08e68f7092a799a992d12c9ad04e328d1426e04a
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-09 08:00:21 +0000

    Comments

--- Task_3/main.py
+++ Task_3/main.py
@@ -1,30 +1,43 @@
 import re
 
-
+# Створюємо функцію для форматування номерів
 def normalize_phone(phone_numbers: list) -> list:
 
+    # Створюємо список в якому будуть готові номери
     formated_numbers = []
 
+    # Перебираємо кожний номер з сирих номерів
     for phone_number in phone_numbers:
+        # Створюємо змінну зі значенням яке шукаємо а саме все окрім "+" та цифр
         pattern = r'[^\+\d]'
+        # Створюємо змінну зі значенням на яке будемо заміняти
         replacement = r''
+        # Видаляємо все з номера окрім "+" та цифр
         formated_number = re.sub(pattern, replacement, phone_number)
 
+        # Перевіряємо чи перший символ "+"
         if formated_number[0] == '+':
+            # Робимо данну операцію для того щоб залишити тільки один "+" якщо їх ненароком більше
             pattern = r'\D'
             formated_number = '+' + re.sub(pattern, replacement, phone_number)
         
+        # Перевіряємо чи первші два символа не 38
+        # Це потрібно для того якщо буде номер з кодом не 380 але вказаний +
+        # То щоб програма не додавала лишнього
         if formated_number[:2] == '38':
+            # Якщо так додаємо "+"
             formated_number = '+' + formated_number
         elif formated_number[0] != '+': 
+            # Якщо ні і не вказано шо цей номер іншої країни то додаємо індекс України
             formated_number = '+38' + formated_number
-            # print(find_ukraine_index)
 
+        # Додаємо готовий номер в список
         formated_numbers.append(formated_number)
-        
+    
+    # Повертаємо список
     return formated_numbers
 
-
+# Список номерів
 phone_numbers = [
     "067\\t123 4567",
     "(095) 234-5678\\n",
@@ -46,4 +59,5 @@ phone_numbers = [
     '+38031231 t1rt  9221737', 
     '+817261 r1r3g2325treg  420034']
 
+# Викликаємо та виводимо результат функції
 print(normalize_phone(phone_numbers))
