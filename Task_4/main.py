8400585e6677019b0fdfcdb8e8a45b2307067c40
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-10 09:07:33 +0000

    Comments

--- Task_4/main.py
+++ Task_4/main.py
@@ -1,22 +1,28 @@
 from datetime import datetime, timedelta
 
+# Створюємо функцію 
 def get_upcoming_birthdays(users: list) -> list:
+    # Створюємо два списки 
+    # Один для днів народження які будуть за 7 днів
     upcoming_birthdays = []
+    # Інший для тих які будут не скоро
     birthdays = []
 
+    # Створюємо змінну today для того шоб позначити сьогоднішню дату
     today = datetime.today().date()
 
+    # Створюємо цикл в якому ми перебираємо кожний елемент переданого списку 
     for user in users:
-        for key, value in user.items():
-            date_time_bd = datetime.strptime(value, '%Y.%m.%d').date()
-            birthday = datetime(year=today.year, month=date_time_bd.month, day=date_time_bd.day).date()
+        key, value = user
+        date_time_bd = datetime.strptime(value, '%Y.%m.%d').date()
+        birthday = datetime(year=today.year, month=date_time_bd.month, day=date_time_bd.day).date()
 
-            if birthday < today:
-                birthdays.append({'name': key, 'congratulating_date': datetime.strftime(birthday, '%Y.%m.%d')})
-            elif birthday.toordinal() - today.toordinal() <= 7:
-                while birthday.weekday() in (5, 6):
-                    birthday += timedelta(days=1)
-                upcoming_birthdays.append({'name': key, 'congratulating_date': datetime.strftime(birthday, '%Y.%m.%d')})
+        if birthday < today:
+            birthdays.append({'name': key, 'congratulating_date': datetime.strftime(birthday, '%Y.%m.%d')})
+        elif birthday.toordinal() - today.toordinal() <= 7:
+            while birthday.weekday() in (5, 6):
+                birthday += timedelta(days=1)
+            upcoming_birthdays.append({'name': key, 'congratulating_date': datetime.strftime(birthday, '%Y.%m.%d')})
     
     return upcoming_birthdays, birthdays
 
